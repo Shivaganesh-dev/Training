f925a097c9127220cb6bbfcc95782228e8d15db5
sanjupatel799 <bandarisanjaykumar8@gmail.com>
2026-06-25 19:08:20 +0530

    updated

---
+++ ayurvena-bootcamp/hour1-2-patient-registr/index.js
@@ -0,0 +1,70 @@
+
+const patients = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 30,
+    disease: "Fever",
+  },
+  {
+    id: 2,
+    name: "Sarah Smith",
+    age: 25,
+    disease: "Cold",
+  },
+  {
+    id: 3,
+    name: "Mike Johnson",
+    age: 40,
+    disease: "Diabetes",
+  },
+    {
+    id: 4,
+    name: " Johnson",
+    age: 35,
+    disease: "Diabetes",
+  },
+];
+
+// it showes the senior patients
+const seniorPatients = patients.filter(
+  patient => patient.age > 35
+);
+
+console.log("Senior Patients:");
+console.log(seniorPatients);
+
+// Display all patients
+console.log("Patient Registry");
+console.log("----------------");
+
+patients.forEach((patient) => {
+  console.log(
+    `ID: ${patient.id}, Name: ${patient.name}, Age: ${patient.age}, Disease: ${patient.disease}`
+  );
+});
+
+
+
+console.log("Patient Registry");
+console.log("----------------");
+
+patients.forEach((patient) => {
+  console.log(
+    `ID: ${patient.id}, Name: ${patient.name}, Age: ${patient.age}, Disease: ${patient.disease}`
+  );
+});
+
+console.log("\nTotal Patients:", patients.length);
+
+const averageAge =
+  patients.reduce((sum, p) => sum + p.age, 0) / patients.length;
+
+console.log("Average Age:", averageAge);
+
+const diabetesPatients = patients.filter(
+  p => p.disease === "Diabetes"
+);
+
+console.log("\nDiabetes Patients:");
+console.log(diabetesPatients);
