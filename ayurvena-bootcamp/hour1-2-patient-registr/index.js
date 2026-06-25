869d6b494f51ec91629f75f1be97c7ecc09b466b
sanjupatel799 <bandarisanjaykumar8@gmail.com>
2026-06-25 19:40:23 +0530

    fullchanges

--- ayurvena-bootcamp/hour1-2-patient-registr/index.js
+++ ayurvena-bootcamp/hour1-2-patient-registr/index.js
@@ -5,26 +5,38 @@ const patients = [
     name: "John Doe",
     age: 30,
     disease: "Fever",
+    emergency: false,
   },
   {
     id: 2,
     name: "Sarah Smith",
     age: 25,
     disease: "Cold",
+    emergency: false,
   },
   {
     id: 3,
     name: "Mike Johnson",
     age: 40,
     disease: "Diabetes",
+    emergency: true,
   },
     {
     id: 4,
     name: " Johnson",
     age: 35,
     disease: "Diabetes",
+    emergency: false,
+  },
+    {
+    id: 5,
+    name: "ram chand",
+    age: 25,
+    disease: "accident",
+    emergency: true,
   },
 ];
+
 
 // it showes the senior patients
 const seniorPatients = patients.filter(
@@ -34,16 +46,6 @@ const seniorPatients = patients.filter(
 console.log("Senior Patients:");
 console.log(seniorPatients);
 
-// Display all patients
-console.log("Patient Registry");
-console.log("----------------");
-
-patients.forEach((patient) => {
-  console.log(
-    `ID: ${patient.id}, Name: ${patient.name}, Age: ${patient.age}, Disease: ${patient.disease}`
-  );
-});
-
 
 
 console.log("Patient Registry");
@@ -54,12 +56,10 @@ patients.forEach((patient) => {
     `ID: ${patient.id}, Name: ${patient.name}, Age: ${patient.age}, Disease: ${patient.disease}`
   );
 });
-
 console.log("\nTotal Patients:", patients.length);
 
 const averageAge =
   patients.reduce((sum, p) => sum + p.age, 0) / patients.length;
-
 console.log("Average Age:", averageAge);
 
 const diabetesPatients = patients.filter(
@@ -68,3 +68,16 @@ const diabetesPatients = patients.filter(
 
 console.log("\nDiabetes Patients:");
 console.log(diabetesPatients);
+
+const emergencyPatients = patients.filter(
+  patient => patient.emergency === true
+);
+
+console.log("Emergency Patients:");
+console.log("-------------------");
+
+emergencyPatients.forEach(patient => {
+  console.log(
+    `ID: ${patient.id}, Name: ${patient.name}, Disease: ${patient.disease}`
+  );
+});
